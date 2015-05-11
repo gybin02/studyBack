74ff2c445e305de4bd2793848c398654b90400a0
gybin02 <gybin02@163.com>
2015-05-11 18:28:09 +0800

    3. add News link to class

--- public/news.js
+++ public/news.js
@@ -1,23 +1,20 @@
 (function(){
 	var module = angular.module("newsModule",[]);
 	module.run(function() {
-	    AV.initialize("po6ihzz17t3wvqgzip3l3vk8fy1ucx0igogu4e3a68f7uhi4", "4higrd4vrv44fhwdeacxhepcjw2w8l4hldp2vogx3n08gcw8");
+	    AV.initialize("5e98i580j085yqa1btnglpq7gykootafi3pwh5q6ldwis3v8", "sey2myyu3qomi0olprmb9of9xi24poigge0pw33sas8qzyyb");
 	});
 	module.controller("newsCtrl",['$http', '$scope', function($http, $scope){
-		var News = AV.Object.extend("news");
-		$scope.news = [];
-	  $scope.newItem = {news_url:'', news_desc:'',image_url:""};
-
-		$scope.editTempItem={};
-
+		var News = AV.Object.extend("msg");
+		$scope.newsArray = [];
+	  $scope.newItem = {content:''};
+	  $scope.editTempItem={};
 
 	  $scope.getItems = function() {
-	  	//var Todo = AV.Object.extend("news");
 	  	var query = new AV.Query(News);
 	  	query.find({
 	  		success:function (results){
 	  			$scope.$apply(function(){
-	  				$scope.news = JSON.parse(JSON.stringify(results));
+	  				$scope.newsArray = JSON.parse(JSON.stringify(results));
 	  			})
 	  		}
 	  	})
@@ -25,16 +22,15 @@
 
 
 	  $scope.addItem = function () {
-	  	//var News = AV.Object.extend("News");
 	  	var news = new News();
-		news.set('news_desc',$scope.newItem.news_desc);
-	  	news.set('news_url',$scope.newItem.news_url);
-		  news.set('image_url',$scope.newItem.image_url);
+		news.set('content',$scope.newItem.content);
+	  	//news.set('datetime',$scope.newItem.news_url);
+		  //news.set('image_url',$scope.newItem.image_url);
 		news.save(null,{
 			success:function(result){
 				$scope.$apply(function(){
-					$scope.news.push(news.toJSON());
-					$scope.newItem={news_url:'',news_desc:'',image_url:""};
+					$scope.newsArray.push(news.toJSON());
+					$scope.newItem={content:''};
 					alert("新增成功");
 				})
 			}
@@ -49,7 +45,7 @@
 				success: function(news){
 					$scope.$apply(function(){
 						alert("删除成功");
-						$scope.news.splice($scope.news.indexOf(newsParam),1);
+						$scope.newsArray.splice($scope.newsArray.indexOf(newsParam),1);
 					})
 				},
 				error:function(news,error){
@@ -58,73 +54,141 @@
 			});
 		};
 
-		$scope.editTemp=function(itemParam){
-			$scope.editTempItem=itemParam;
-		};
-
-		$scope.editItem=function(){
-			//var News =AV.Object.extend("News");
-			//$scope.editTempItem.save
-			var news = new News();
-			news.set("objectId",$scope.editTempItem.objectId);
-			news.set('news_desc',$scope.editTempItem.news_desc);
-			news.set('news_url',$scope.editTempItem.news_url);
-			news.set('image_url',$scope.editTempItem.image_url);
-
-			news.save(null,{
-				success:function(news){
-					alert("编辑成功");
-					$('#myModal').modal('hide');
-					console.error("success Edit")
-				},
-				error:function(news){
-					console.error("fail edit");
-				}
-			});
-		};
-
-		$scope.denyCar=function(itemParam){
-
-		};
+		//$scope.editTemp=function(itemParam){
+		//	$scope.editTempItem=itemParam;
+		//};
+        //
+		//$scope.editItem=function(){
+		//	//var News =AV.Object.extend("News");
+		//	//$scope.editTempItem.save
+		//	var news = new News();
+		//	news.set("objectId",$scope.editTempItem.objectId);
+		//	news.set('news_desc',$scope.editTempItem.news_desc);
+		//	news.set('news_url',$scope.editTempItem.news_url);
+		//	news.set('image_url',$scope.editTempItem.image_url);
+        //
+		//	news.save(null,{
+		//		success:function(news){
+		//			alert("编辑成功");
+		//			$('#myModal').modal('hide');
+		//			console.error("success Edit")
+		//		},
+		//		error:function(news){
+		//			console.error("fail edit");
+		//		}
+		//	});
+		//};
+        //
+		//$scope.denyCar=function(itemParam){
+        //
+		//};
 
     $scope.getItems();
 
 	}]);
-	module.controller('CarCtrl', function($scope) {
-		var Car=AV.Object.extend("Cars");
-		$scope.cars = [];
+	module.controller('ClassCtrl', function($scope) {
+		var Subject=AV.Object.extend("subject");
+		var CategorySubject=AV.Object.extend("category_subject");
+		var Category=AV.Object.extend("category");
 
-		$scope.getCars = function() {
-			var query = new AV.Query(Car);
-			query.equalTo("status",2);
+		$scope.subjectArray = [];
+		$scope.subjectTemp = {};
+		$scope.selected={};
+
+		$scope.categorySubjectArray =[];
+
+		$scope.categoryArray = [];
+
+		$scope.getCategorySubject = function() {
+			var query = new AV.Query(CategorySubject);
 			query.find({
 				success:function (results){
 					$scope.$apply(function(){
-						$scope.cars = JSON.parse(JSON.stringify(results));
+						$scope.categorySubjectArray = JSON.parse(JSON.stringify(results));
 					})
 				}
 			})
 		};
 
-
-		$scope.passCar=function(itemParam){
-			var car = new Car();
-			car.set("objectId",itemParam.objectId);
-			car.set("status",1);
-			car.save(null,{
-				success: function () {
-					alert('审核通过');
+		$scope.getCategory = function() {
+			var query = new AV.Query(Category);
+			query.find({
+				success:function (results){
 					$scope.$apply(function(){
-						$scope.cars.splice($scope.cars.indexOf(itemParam),1);
+						$scope.CategoryArray = JSON.parse(JSON.stringify(results));
+					})
+				}
+			})
+		};
+
+		$scope.getSubject = function() {
+			var query = new AV.Query(Subject);
+			query.find({
+				success:function (results){
+					$scope.$apply(function(){
+						$scope.subjectArray = JSON.parse(JSON.stringify(results));
+					})
+				}
+			})
+		};
+
+		$scope.addSubject = function () {
+			var subject = new Subject();
+			subject.set('teacher',$scope.subjectTemp.teacher);
+			subject.set('place',$scope.subjectTemp.place);
+			subject.set('categorysubject_id',$scope.selected.objectId);
+			subject.set('course_arrangement',$scope.subjectTemp.course_arrangement);
+			subject.set('schooltime',$scope.subjectTemp.schooltime);
+			subject.set('teacher_description',$scope.subjectTemp.teacher_description);
+			subject.save(null,{
+				success:function(result){
+					$scope.$apply(function(){
+						$scope.subjectArray.push(subject.toJSON());
+						$scope.subjectTemp={};
+						alert("新增成功");
+					})
+				}
+			});
+		};
+
+		$scope.deleteSubject=function(itemParam){
+			var subject = new Subject();
+			subject.set("objectId",itemParam.objectId);
+			subject.destroy({
+				success: function(){
+					$scope.$apply(function(){
+						alert("删除成功");
+						$scope.subjectArray.splice($scope.subjectArray.indexOf(itemParam),1);
 					})
 				},
 				error:function(){
-					console.error("fail Pass Audit");
+					console.error("fail delete");
 				}
-			})
+			});
 		};
 
-		$scope.getCars();
+
+		$scope.deleteCategorySubject=function(itemParam){
+			var CategorySubject = new CategorySubject();
+			CategorySubject.set("objectId",itemParam.objectId);
+			CategorySubject.destroy({
+				success: function(){
+					$scope.$apply(function(){
+						alert("删除成功");
+						$scope.CategorySubjectArray.splice($scope.CategorySubjectArray.indexOf(itemParam),1);
+					})
+				},
+				error:function(){
+					console.error("fail delete");
+				}
+			});
+		};
+
+		$scope.test=function(itemParam){
+			console.error("Hello");
+		}
+		$scope.getCategorySubject();
+		$scope.getSubject();
 
 	})
 
